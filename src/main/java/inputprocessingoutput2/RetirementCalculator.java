5881e714f307b56ffd7b86ae86ec574048817c29
GergelyIstvanRobert <skiridershop@gmail.com>
2019-12-10 12:05:36 +0200

    corectat3push

--- src/main/java/inputprocessingoutput2/RetirementCalculator.java
+++ src/main/java/inputprocessingoutput2/RetirementCalculator.java
@@ -1,5 +1,23 @@
 package inputprocessingoutput2;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class RetirementCalculator {
+    public static void main(String[] args, int result){
+        int currentAge,retireAge;
+        Scanner keyboard1 = new Scanner(System.in);
+        System.out.print("What is your current age? ");
+        currentAge=keyboard1.nextInt();
+        System.out.print("At what age would you like to retire? ");
+        retireAge=keyboard1.nextInt();
+        result = retireAge-currentAge;
+        System.out.println("You have " +result+ " years left until you can retire. ");
+        int thisYear= Calendar.getInstance().get(Calendar.YEAR);
+        result = thisYear+ retireAge-currentAge;
+        System.out.println("It's" + thisYear + "so you can retire in " + result );
+
+    }
+
 
 }
